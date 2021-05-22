f4f5b7b6494aac7e7d0873935298934908961fc9
Austin Wallace <austinwallace196@gmail.com>
2021-05-22 12:41:10 -0500

    pre-update

--- ice_manager.py
+++ ice_manager.py
@@ -319,6 +319,89 @@ def generateGraph(spec_name, T, title, filename, x_range=[100,300], x_units='nm'
 
     return
 
+def generateMultiGraph(methods_lst, spec_name, T, title, filename, x_range=[100,300], x_units='nm', peaks=False):
+    #print(os.getcwd())
+    fig, ax1 = plt.subplots()
+
+    data = np.genfromtxt("results/final/data/" + spec_name, delimiter=" ")
+    data = data.tolist()
+    # print(data)
+    x = []
+    y = []
+    highest_y = 0
+    for i in data:
+        #print(i)
+        x.append(i[0])
+        y.append(i[1])
+        if i[1] > highest_y:
+            highest_y = i[1]
+    
+    for i in range(len(y)):
+        y[i] /= highest_y
+    if x_units == 'eV' or x_units=='ev':
+        h = 6.626E-34
+        c = 3E17
+        ev_to_joules = 1.60218E-19
+        x = [ h*c/(i*ev_to_joules) for i in x ]
+        x.reverse()
+        y.reverse()
+    elif x_units == 'cm-1':
+        x.reverse()
+        y.reverse()
+        #maxima = scipy.signal.argrelextrema(y, np.greater)
+        
+    # print(x)
+    #print('\n', y)
+    ax1.plot(x, y, "k-", label="T = {0} K".format(T))
+    #ax1.set_xlim([x[0], x[-1]])
+    ax1.set_xlim(x_range)
+    ax1.set_ylim(0, 1.2)
+
+    plt.title(title)
+    if x_units == 'ev' or x_units=='eV':
+        print(x)
+        plt.xlabel("Electronvolts (eV)")
+        ax1.legend(shadow=True, fancybox=True)
+        if peaks:
+            arr_y = np.array(y)
+            print("local maxima")
+            peaks, _ = scipy.signal.find_peaks(arr_y, height=0)
+            for i in peaks:
+                print(round(x[i],2), arr_y[i])
+                height = arr_y[i]
+                frequency = round(x[i], 2)
+                if height > 0.02:
+                    plt.text(frequency - 0.08, arr_y[i]+0.05, '%.2f' % frequency )
+
+    elif x_units == 'cm-1':
+        plt.xlabel(r"Wavenumbers cm$^{-1}$")
+        if peaks:
+            arr_y = np.array(y)
+            print("local maxima")
+            peaks, _ = scipy.signal.find_peaks(arr_y, height=0)
+            for i in peaks:
+                print(round(x[i]), arr_y[i])
+                height = arr_y[i]
+                frequency = round(x[i])
+                if height > 0.02:
+                    plt.text(frequency+100, arr_y[i]+0.1, '%d' % frequency )
+    else:
+        plt.xlabel("Wavelength (nm)")
+        ax1.legend(shadow=True, fancybox=True)
+
+    plt.ylabel("Oscillator Strength")
+    plt.grid(b=None, which='major', axis='y', linewidth=1)
+    plt.grid(b=None, which='major', axis='x', linewidth=1)
+    # os.chdir("results/final/graphs")
+    os.chdir("results/final")
+    if "graphs" not in glob.glob("graphs"):
+        os.mkdir("graphs")
+    os.chdir("graphs")
+    plt.savefig(filename)
+    os.chdir("../../..")
+
+    return
+
 
 def main():
     mol_xyz1 = "mon_nh3.xyz"
@@ -340,6 +423,7 @@ def main():
     mem_com_opt = "1600"  # mb
     mem_pbs_opt = "15"  # gb
 
+    methods_lst = ["B3LYP", "PBE0", "wB97XD", "CAM-B3LYP", "B3LYPD3", "B97D3"]
     # TD-DFT options
     #method_mexc = "B3LYP"
     #method_mexc = "PBE0"
